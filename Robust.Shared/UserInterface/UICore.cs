5c4f129defe45a687593e9145bea3cb0fc716159
Jezithyr <Jezithyr@gmail.com>
2023-12-27 23:08:45 -0800

    Started work on UIAssembly loading

---
+++ Robust.Shared/UserInterface/UICore.cs
@@ -0,0 +1,17 @@
+﻿using Robust.Shared.IoC;
+
+namespace Robust.Shared.UserInterface;
+
+public interface IUICallback;
+
+public abstract class UICore
+{
+    public abstract void RegisterIoC(IDependencyCollection deps);
+
+    public abstract void Initialize();
+
+    public abstract void PostInit();
+
+    public abstract void Unload();
+
+}
